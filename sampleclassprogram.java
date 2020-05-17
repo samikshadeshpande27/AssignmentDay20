54c883d9d3bb039aebb643d1b1a987b81e60e465
samiksha <samikshadeshpande27@gmail.com>
2020-05-17 06:32:22 -0700

    Day20

---
+++ sampleclassprogram.java
@@ -0,0 +1,35 @@
+public class sampleclassprogram 
+{
+private final double rx,ry;
+private final double q;
+public sampleclassprogram(double x0,double y0,double q0)
+{
+	rx=x0;
+	ry=y0;
+	q=q0;
+	
+}
+public double potentialAt(double x, double y)
+{
+	double k = 8.99e09;
+	double dx = x - rx;
+	double dy = y - ry;
+	return k * q / Math.sqrt(dx*dx + dy*dy);
+}
+public String toString()
+{
+	return q + " at " + "("+ rx + ", " + ry +")";
+}
+	public static void main(String[] args) 
+	{
+		double x = Double.parseDouble(args[0]);
+		double y = Double.parseDouble(args[1]);
+		sampleclassprogram c1 = new sampleclassprogram(0.51,0.63,21.3);
+		sampleclassprogram c2 = new sampleclassprogram(0.13,0.94,81.9);
+		double v1 = c1.potentialAt(x, y);
+		double v2 = c2.potentialAt(x, y);
+		System.out.printf("%.2e\n",(v1 + v2));
+	}
+}
+
+
